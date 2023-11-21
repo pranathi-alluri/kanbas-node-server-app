7b6ac1506f773839ed10fef5b8e44b4eabd68d2e
Pranathi13 <alluri.p@northeastern.edu>
2023-11-20 23:14:11 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from 'express';
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import cors from "cors";
+const app = express()
+app.use(cors());
+app.use(express.json())
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app);
+Hello(app)
+app.listen(process.env.PORT || 4000);
